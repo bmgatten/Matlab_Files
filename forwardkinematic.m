7da5b77535806df6ef794250ab110a11c20aa7e7
bmgatten <bmgatten@ucdavis.edu>
2018-04-09 13:04:28 -0700

    EBS 289K Assignment1

---
+++ forwardkinematic.m
@@ -0,0 +1,27 @@
+%% Lecture 3: Implement differential drive forward kinematic models %%
+function [x, y, theta] = forwardkinematic(wr, wl, Rw)
+%Set relevant parameters
+l = 1; %m
+r_w = 0.5; %m
+dt = .05; %s (use .1, .5s later)
+
+%set inital conditions
+x=0;
+y=0;
+theta=0;
+
+v_r = wr*r_w; %right wheel velocity
+v_l = wl*r_w; %left wheel velocity
+V = (v_r+v_l)/2; %Forward Velocity
+w = (v_r-v_l)/l; %Rotational Velocity
+
+%Euler's Method
+x = x + dt*V*cos(theta);
+y = y + dt*V*sin(theta);
+theta = theta+ dt*w;
+
+%Integration of forward kinematics
+R = L/w*((v_l+v_r)/(v_r-v_l)); %Distance from center of the car to the Icc
+%horizontal position in the world frame
+Iccx = x-R*sin(theta);%horizontal position of center of rotation
+Iccy = y+R*cos(theta);%vertical position of center of rotation
