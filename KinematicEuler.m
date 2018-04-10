aeab7c540401e5f4bcfe6c6977f05443a1c6e315
benjamingatten <benjamingatten@campus-012-067.ucdavis.edu>
2018-04-09 19:53:29 -0700

    Returns cartesian position and orientation

---
+++ KinematicEuler.m
@@ -0,0 +1,14 @@
+function [x, y, theta] = KinematicEuler(x,y,theta,wl, wr, r_w,l,dt) %Accept Rad/s as inputs for wl and wr
+%KinematicEuler determines a new position vector from rotational wheel
+%velocities and time steps
+
+%Calculate Velocities
+v_r = wr*r_w; %right wheel velocity
+v_l = wl*r_w; %left wheel velocity
+V = (v_r+v_l)/2; %Forward Velocity
+w = (v_r-v_l)/l; %Rotational Velocity
+%Euler's Method
+theta = theta+ dt*w; %rad
+x = x + dt*V*cos(theta); %m
+y = y + dt*V*sin(theta); %m
+end
