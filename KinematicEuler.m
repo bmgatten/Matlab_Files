00537d170ac4b598933c815d6b391f316cca11ad
bmgatten <bmgatten@ucdavis.edu>
2018-04-24 17:24:04 -0700

    Simple Euler Integration/Equations for Differential Drive Robot

--- KinematicEuler.m
+++ KinematicEuler.m
@@ -8,7 +8,7 @@ v_l = wl*r_w; %left wheel velocity
 V = (v_r+v_l)/2; %Forward Velocity
 w = (v_r-v_l)/l; %Rotational Velocity
 %Euler's Method
-theta = theta+ dt*w; %rad
 x = x + dt*V*cos(theta); %m
 y = y + dt*V*sin(theta); %m
+theta = theta+ dt*w; %rad
 end
